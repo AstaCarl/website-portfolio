d341b9d778ec98e646ae970a449c3441b24af679
AstaCarl <cph-ac300@cphbusiness.dk>
2026-05-28 15:07:32 +0200

    made tools responsive

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -14,6 +14,7 @@ import MongoDBIcon from "./icons/mongoDB";
 import FigmaIcon from "./icons/figma";
 import DenoIcon from "./icons/deno";
 import ExpressJSIcon from "./icons/express";
+import DisplayTitle from "./DisplayTitle";
 
 type ToolsProps = {
   displayTitle: string;
@@ -23,7 +24,7 @@ const tools = [
   {
     tool: "React",
     color: "var(--color-red)",
-    icon: <ReactIcon  color="var(--color-creme)"/>,
+    icon: <ReactIcon color="var(--color-creme)" />,
     textColor: "var(--color-creme)",
   },
   {
@@ -33,7 +34,7 @@ const tools = [
     textColor: "var(--color-creme)",
   },
   {
-    tool: "Git Version Control",
+    tool: "Git",
     color: "var(--color-light-red)",
     icon: <GitIcon />,
   },
@@ -54,42 +55,72 @@ const tools = [
     textColor: "var(--color-dark-blue)",
   },
   {
-    tool: "CSS 3",
-    color: "var(--color-light-red)",
-    icon: <CSSicon />,
-    textColor: "var(--color-dark-blue)",
-  },
-
-  {
     tool: "PostgreSQL",
     color: "var(--color-green)",
     icon: <PostgresIcon color="var(--color-creme)" />,
     textColor: "var(--color-creme)",
   },
-  { tool: "NestJS", color: "var(--color-light-red)", icon: <NestJSIcon color="var(--color-dark-blue)"/>, textColor: "var(--color-dark-blue)" },
-  { tool: "Next.js", color: "var(--color-yellow)", icon: <NextJSIcon />, textColor: "var(--color-dark-blue)" },
-  { tool: "MongoDB", color: "var(--color-light-blue)", icon: <MongoDBIcon color="var(--color-dark-blue)"/>, textColor: "var(--color-dark-blue)" },
-  { tool: "Figma", color: "var(--color-yellow)", icon: <FigmaIcon />, textColor: "var(--color-dark-blue)" },
-    { tool: "Express.js", color: "var(--color-green)", icon: <ExpressJSIcon />, textColor: "var(--color-creme)" },
-    { tool: "Deno", color: "var(--color-red)", icon: <DenoIcon color="var(--color-creme)" />, textColor: "var(--color-creme)" },
+
+  {
+    tool: "NestJS",
+    color: "var(--color-light-red)",
+    icon: <NestJSIcon color="var(--color-dark-blue)" />,
+    textColor: "var(--color-dark-blue)",
+  },
+  {
+    tool: "Deno",
+    color: "var(--color-red)",
+    icon: <DenoIcon color="var(--color-creme)" />,
+    textColor: "var(--color-creme)",
+  },
+  {
+    tool: "Next.js",
+    color: "var(--color-yellow)",
+    icon: <NextJSIcon />,
+    textColor: "var(--color-dark-blue)",
+  },
+  {
+    tool: "MongoDB",
+    color: "var(--color-light-blue)",
+    icon: <MongoDBIcon color="var(--color-dark-blue)" />,
+    textColor: "var(--color-dark-blue)",
+  },
+
+  {
+    tool: "CSS 3",
+    color: "var(--color-light-red)",
+    icon: <CSSicon />,
+    textColor: "var(--color-dark-blue)",
+  },
+  {
+    tool: "Figma",
+    color: "var(--color-yellow)",
+    icon: <FigmaIcon />,
+    textColor: "var(--color-dark-blue)",
+  },
+  {
+    tool: "Express.js",
+    color: "var(--color-green)",
+    icon: <ExpressJSIcon />,
+    textColor: "var(--color-creme)",
+  },
 ];
 
 const row1 = tools.slice(0, 2);
 const row2 = tools.slice(2, 5);
 const row3 = tools.slice(5, 7);
-const row4 = tools.slice(7, 11);
-const row5 = tools.slice(11, 14);
+const row4 = tools.slice(7, 10);
+const row5 = tools.slice(10, 11);
+const row6 = tools.slice(11, 14);
 
 export default function Tools({ displayTitle }: ToolsProps) {
   return (
-    <section className="flex flex-col gap-8 p-60 lg:px-80 flex items-center">
-      <h1 className="text-gray-blue font-primary font-bold text-8xl text-center">
-        {displayTitle}
-      </h1>
+    <section className="flex flex-col gap-8 px-10 py-30 md:px-30 lg:px-80 items-center">
+      <DisplayTitle title={displayTitle} />
       <Title variant="h2" title="Technologies and Platforms I've Mastered" />
 
       <div className="flex items-center flex-col gap-4">
-        <div className="flex gap-6">
+        <div className="flex gap-2 md:gap-6">
           {row1.map((t) => (
             <ToolLabel
               key={t.tool}
@@ -101,7 +132,7 @@ export default function Tools({ displayTitle }: ToolsProps) {
             />
           ))}
         </div>
-        <div className="flex gap-6">
+        <div className="flex gap-2 md:gap-6">
           {row2.map((t) => (
             <ToolLabel
               key={t.tool}
@@ -113,7 +144,7 @@ export default function Tools({ displayTitle }: ToolsProps) {
             />
           ))}
         </div>
-        <div className="flex gap-6">
+        <div className="flex gap-2 md:gap-6">
           {row3.map((t) => (
             <ToolLabel
               key={t.tool}
@@ -125,7 +156,7 @@ export default function Tools({ displayTitle }: ToolsProps) {
             />
           ))}
         </div>
-        <div className="flex gap-6">
+        <div className="flex gap-2 md:gap-6">
           {row4.map((t) => (
             <ToolLabel
               key={t.tool}
@@ -137,8 +168,20 @@ export default function Tools({ displayTitle }: ToolsProps) {
             />
           ))}
         </div>
-        <div className="flex gap-6">
+        <div className="flex gap-2 md:gap-6">
           {row5.map((t) => (
+            <ToolLabel
+              key={t.tool}
+              tool={t.tool}
+              color={t.color}
+              icon={t.icon}
+              textColor={t.textColor}
+              size="medium"
+            />
+          ))}
+        </div>
+        <div className="flex gap-2 md:gap-6">
+          {row6.map((t) => (
             <ToolLabel
               key={t.tool}
               tool={t.tool}
