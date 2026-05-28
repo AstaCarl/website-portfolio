ce3f30e35df42f4028b8d84bdb09140e97ca7cf7
AstaCarl <cph-ac300@cphbusiness.dk>
2026-05-28 14:07:10 +0200

    made header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,8 @@ import Title from "./Title";
 
 export default function Header() {
     return (
-        <header className="bg-light-blue w-full h-20 rounded-b-4xl flex justify-between px-60 lg:px-80 items-center">
+        <header className="bg-light-blue w-full h-15 rounded-b-4xl flex justify-between px-10 md:px-30 lg:px-80 items-center">
             <Title variant="h3" title="Asta Carl." />
-            {/* <nav className="flex gap-4">
-                <a href="">Projects</a>
-                <a href="">Tools</a>
-                <a href="">Connect</a>
-            </nav> */}
         </header>
     );
 }
