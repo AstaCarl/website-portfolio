c07374bb61e8e277241bb1d1b745c7dd3fc99abb
AstaCarl <cph-ac300@cphbusiness.dk>
2026-05-28 14:42:44 +0200

    updated styles for responsivenes

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -2,7 +2,7 @@
 
 export default function Paragraph({ children, variant }: { children: React.ReactNode; variant?: string }) {
   return (
-    <p className={`text-gray-blue ${variant === "code" ? "font-code" : "font-primary"}`}>
+    <p className={`text-gray-blue text-xs md:text-base ${variant === "code" ? "font-code" : "font-primary"}`}>
       {children}
     </p>
   );
