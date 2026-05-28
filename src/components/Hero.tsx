8c7e78bd15bce69b4ccaf5d30417abc8487a91f8
AstaCarl <cph-ac300@cphbusiness.dk>
2026-05-28 14:26:05 +0200

    made hero banner responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,10 +10,10 @@ type HeroProps = {
 
 export default function Hero({ subtitle, displayTitle, title, paragraph }: HeroProps) {
   return (
-    <section className="w-[80%] flex flex-col gap-8 p-60 lg:px-80">
+    <section className="px-10 py-30 flex flex-col gap-4 md:gap-6 lg:gap-8 md:p-30 lg:p-80">
         <div>
       <Title variant="h3" title={subtitle} />
-      <h1 className="text-gray-blue font-primary font-bold text-8xl">
+      <h1 className="text-gray-blue font-primary font-bold text-5xl md:text-7xl lg:text-8xl">
         {displayTitle}
       </h1>
       </div>
